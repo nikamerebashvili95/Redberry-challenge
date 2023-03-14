1e45e360d109b5fb11d36ff0ec9175dcc58c64e3
Nika Merebashvili <114658102+nikamerebashvili95@users.noreply.github.com>
2023-03-14 07:37:12 -0700

    work with styling and button functions

---
+++ app.js
@@ -0,0 +1,9 @@
+// მთავარი გვერდიდან გადასვლა შესავსებ აპლიკაციაში
+function goToGeneralInfo() {
+  location.href = "./survey.html";
+}
+// მთავარ გვერდზე დასაბრუნებელი ღილაკის ფუნქცია
+function goToLanding() {
+  location.href = "./index.html";
+  localStorage.clear();
+}
