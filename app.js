26ff3ceec20078ff5fbd1af0f995b23a067def47
Nika Merebashvili <114658102+nikamerebashvili95@users.noreply.github.com>
2023-03-14 09:35:20 -0700

    work with img upload styling and functions

--- app.js
+++ app.js
@@ -7,3 +7,38 @@ function goToLanding() {
   location.href = "./index.html";
   localStorage.clear();
 }
+// ფოტოს ატვირთვა და გამოჩენა მარჯვნივ
+const fileInput = document.getElementById("image");
+const imageColumn = document.querySelector(".resume--photo");
+const imgValidIcon = document.querySelector(".img-valid-icon");
+const imglInvalidIcond = document.querySelector(".img-invalid-icon");
+fileInput.addEventListener("change", updateResumeImg);
+fileInput.addEventListener("input", saveFile);
+function saveFile(e) {
+  const image = e.target.files[0];
+  if (image && image["type"].split("/")[0] === "image") {
+    imgValidIcon.classList.add("show");
+    imglInvalidIcond.classList.remove("show");
+    const reader = new FileReader();
+    reader.readAsDataURL(image);
+    reader.addEventListener("load", () => {
+      localStorage.setItem("image", reader.result);
+    });
+  } else {
+    imgValidIcon.classList.remove("show");
+    imglInvalidIcond.classList.add("show");
+  }
+}
+function updateResumeImg() {
+  const file = this.files[0];
+  const reader = new FileReader();
+
+  reader.addEventListener("load", function () {
+    const img = new Image();
+    img.src = reader.result;
+
+    imageColumn.innerHTML = `<img src=${reader.result} class='resume--image'>`;
+  });
+
+  reader.readAsDataURL(file);
+}
